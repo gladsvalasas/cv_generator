252934b5f9975e0d78a98176c44e4e3259932587
m1n64 <mrcaxapov@gmail.com>
2022-04-15 01:25:26 +0300

    GT-21 fixes

--- app/Models/Landing/PortfolioProjects.php
+++ app/Models/Landing/PortfolioProjects.php
@@ -2,10 +2,17 @@
 
 namespace App\Models\Landing;
 
+use App\Interfaces\IVerificationModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PortfolioProjects extends Model
-{
+class PortfolioProjects extends Model implements IVerificationModel{
     use HasFactory;
+
+    public static function getValidatorTemplate()
+    {
+        return [
+
+        ];
+    }
 }
